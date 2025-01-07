01759a9a4854cdb137229a71501b0eee4583f19c
elnitrocl <87839744+elnitrocl@users.noreply.github.com>
2025-01-07 11:59:59 -0300

    Minor correct at  fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,8 +3,8 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Nitro'
-description 'Sistema optimizado de trabado de armas para QBox con soporte para idiomas y deterioro'
-version '2.0.0'
+description 'Weapon Jam System'
+version '1.0.0'
 lua54 'yes' -- Habilita Lua 5.4 para este recurso
 
 shared_scripts {
